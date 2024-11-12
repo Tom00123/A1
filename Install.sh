5cae0cf6648a5d05d5785764ab7170507c51839b
Tom00123 <94983697+Tom00123@users.noreply.github.com>
2024-11-12 09:27:57 +0700

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+apt update
+
+apt full-upgrade -y
+
+pkg install libjansson wget nano -y
+
+mkdir ccminer && cd ccminer
+
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/main/ccminer
+wget https://raw.githubusercontent.com/Tom00123/A1/refs/heads/main/Config.json
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/main/start.sh
+
+chmod +x ccminer start.sh
